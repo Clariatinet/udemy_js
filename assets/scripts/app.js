06e517c5aec2ec53315a5ba6aedb68a8cd91719a
sungmin <95615086+Clariatinet@users.noreply.github.com>
2023-11-07 22:51:11 +0900

    ㅁㅁㅁㅁ

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -68,9 +68,6 @@ function divide() {
 
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
-addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
