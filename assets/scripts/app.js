b15474e3f6961e1412b14de991de9975cd681b5c
sungmin <95615086+Clariatinet@users.noreply.github.com>
2023-11-20 23:48:25 +0900

    ㅁㅁ

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -70,3 +70,4 @@ addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
 divideBtn.addEventListener('click', divide);
+divideBtn.addEventListener('click', divide);
